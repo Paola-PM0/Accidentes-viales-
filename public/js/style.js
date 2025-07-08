8828a4fa75bb67aafcd80721eba85fc525130bc2
Paola <Paola_Perez_13@hotmail.com>
2025-07-08 12:01:22 -0600

    migracion vue con vite

--- public/js/style.js
+++
@@ -1,9 +0,0 @@
- // Mostrar/Ocultar Panel
-    const toggleBtn = document.getElementById('toggleDashboardBtn');
-    const dashboard = document.getElementById('dashboardPanel');
-
-    toggleBtn.addEventListener('click', () => {
-    const visible = dashboard.style.display !== 'none';
-    dashboard.style.display = visible ? 'none' : 'flex';
-    toggleBtn.textContent = visible ? 'Mostrar Panel' : 'Ocultar Panel';
-    });
